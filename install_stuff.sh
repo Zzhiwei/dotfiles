4f351b404b171e1567c770bd9bcb3a531ef82df4
Lin Zhiwei <zhiwei.lin@viki.com>
2024-08-17 22:48:01 +0800

    date

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,4 +1,4 @@
-
+#! /bin/bash
 
 ############
 # terminal
@@ -12,7 +12,6 @@ echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 brew install zsh-syntax-highlighting
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
-
 ############
 # git
 ############
@@ -20,15 +19,17 @@ brew install --cask git-credential-manager
 
 # for token based auth
 # printf "host=github.com\nprotocol=https\nusername=$USERNAME\npassword=$TOKEN" | git credential-manager store
-
+# for ssh
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 
 ############
-# apps
+# others
 ############
+
+# install apps
 xargs brew install < apps.txt
 
-
-# vscode
+# add vscode to terminal
 cat << EOF >> ~/.zprofile
 # Add Visual Studio Code (code)
 export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
