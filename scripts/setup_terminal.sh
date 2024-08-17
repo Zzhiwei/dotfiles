0f9a135e3a4f554fa4235ef52ae38ab58cd512bc
Lin Zhiwei <zhiwei.lin@viki.com>
2024-08-17 20:32:55 +0800

    add script

---
+++ scripts/setup_terminal.sh
@@ -0,0 +1,10 @@
+
+brew install zoxide
+brew install eza
+
+brew install zsh-autosuggestions
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+brew install zsh-syntax-highlighting
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
